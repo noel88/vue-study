ef13c349e9099ede53d41500e1487269afd2aa6a
xiah <xiah@rtruesoft.kr>
2019-11-22 11:20:41 +0900

    day2

---
+++ day2/app.js
@@ -0,0 +1,22 @@
+
+Vue.component('todo-footer', {
+    template: '<p>This is another global child component</p>'
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'This is a parent component'
+    }
+});
+
+var cmp = {
+    template: '<p>This is another local child component</p>'
+};
+
+new Vue({
+    el: '#app',
+    components: {
+        'todo-list': cmp
+    }
+});
