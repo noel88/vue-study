21a9737dc49d3dfc2214601d91869497c3fc1519
n <xiah@rtruesoft.kr>
2019-11-24 16:12:20 +0900

    day4

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+
+</template>
+
+<script>
+import TodoHeader from "./component/TodoHeader";
+import TodoFooter from "./component/TodoFooter";
+import TodoInput from "./component/TodoInput";
+import TodoList from "./component/TodoList";
+
+export default {
+    components: {
+        'TodoHeader' : TodoHeader,
+        'TodoFooter' : TodoFooter,
+        'TodoInput' : TodoInput,
+        'TodoList' : TodoList,
+    }
+}
+</script>
+
+<style>
+
+</style>
